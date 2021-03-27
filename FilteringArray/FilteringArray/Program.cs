6ab0902c493a11b6a49ec35405dbcceb205bd59f
Lotus19955 <lotus19955@gmail.com>
2021-03-27 12:42:46 +0300

    comments

---
+++ FilteringArray/FilteringArray/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FilteringArrayLib;
+
+namespace FilteringArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] MyArray = new int[] { 1, 23, 54, 67, 6, 90, 5, 7, 33, 73, 98, 55887, 38, 369, 95894, 561279, 12345637, 542987, 7774 };
+            int IntWhatWeFinde = 7;
+
+            ArrayMethods.ArraySorting(MyArray);
+            for (int i = 0; i < MyArray.Length; i++)
+            {
+                Console.Write($"{MyArray[i]}\t");
+            }
+
+            ArrayMethods.FindeNumeralInArray(MyArray,IntWhatWeFinde);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ FilteringArray/FilteringArrayLib/FilteringArrayLib.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilteringArrayLib
+{
+    public class ArrayMethods
+    {
+        /// <summary>
+        /// Метод сортировки массива целых чисел от меньшего к большему. Принимает массив целых чисел.
+        /// </summary>
+        /// <param name="MyArray">Массив целых чисел</param>
+        public static void ArraySorting(int[] MyArray)
+        {
+            for (int i = 0; i < MyArray.Length; i++)
+            {
+                for (int y = i + 1; y < MyArray.Length; y++)
+                {
+                    if (MyArray[i] > MyArray[y])
+                    {
+                        int num = MyArray[i];
+                        MyArray[i] = MyArray[y];
+                        MyArray[y] = num;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Метод поиска значение в массиве. Принимает 2 параметра: 1)массив целых чисел; 2)число которое нужно найти.
+        /// </summary>
+        /// <param name="MyArray">Массив целых чисел</param>
+        /// <param name="IntWhatWeFinde">Число которое необходимо найти</param>
+        public static void FindeNumeralInArray(int[] MyArray, int IntWhatWeFinde)
+        {
+            char StrWhatWeFinde = Char.Parse(IntWhatWeFinde.ToString());
+            string[] StrArray = new string[MyArray.Length];
+            for (int i = 0; i < MyArray.Length; i++)
+            {
+                StrArray[i] = MyArray[i].ToString();
+            }
+            for (int i = 0; i < StrArray.Length; i++)
+            {
+                if (StrArray[i].Contains(StrWhatWeFinde))
+                {
+                    int ValueForShow = int.Parse(StrArray[i]);
+                    Console.WriteLine(ValueForShow);
+                }
+            }
+        }
+        /// <summary>
+        /// Метод проверки на заполненяемость массива; Принимает массив челых чисел.
+        /// </summary>
+        /// <param name="MyArray">Массив целых чисел</param>
+        private static void CheckArrayForNull (int [] MyArray)
+        {
+            for (int i = 0; i < MyArray.Length; i++)
+            {
+                if (MyArray[i] == 0)
+                {
+                    Console.WriteLine($"Индекс массив {MyArray[i]} равен 0");
+                }
+                if (i == MyArray.Length)
+                {
+                    break;
+                }
+            }
+        }//не особо понял задание + как вывести "privat" в мейн
+        /// <summary>
+        /// Метод проверяет длинну массива на 20 индексов
+        /// </summary>
+        /// <param name="MyArray">Массив целых чисел</param>
+        private static void CheckArrayForLength(int[] MyArray)
+        {
+            if (MyArray.Length > 20)
+            {
+                Console.WriteLine($"Массив MyArray не прошел проверку на вместимость");
+                Console.WriteLine("Массив будет увеличен на 1 индекс");
+                Array.Resize(ref MyArray, MyArray.Length + 1);
+            }
+        } //не особо понял задание + как вывести "privat" в мейн
+    }
+}
+
